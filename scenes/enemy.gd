2374ff72574666c2ff3291209252cdd7bf0c3fcc
vib <vibrunazo@gmail.com>
2024-05-25 10:32:10 -0300

    enemy moving

---
+++ scenes/enemy.gd
@@ -0,0 +1,11 @@
+extends PathFollow2D
+
+@export var SPEED = 100.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _physics_process(delta):
+	progress += delta * SPEED
