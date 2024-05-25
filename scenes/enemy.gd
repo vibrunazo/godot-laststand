db62282f3242fabe0c4191c142854a0ba742678d
vib <vibrunazo@gmail.com>
2024-05-25 12:04:58 -0300

    smooth path follow and flip sprite

--- scenes/enemy.gd
+++ scenes/enemy.gd
@@ -1,6 +1,8 @@
 extends PathFollow2D
 
 @export var SPEED = 100.0
+@onready var sprite = $Sprite
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,3 +11,10 @@ func _ready():
 
 func _physics_process(delta):
 	progress += delta * SPEED
+
+func _process(_delta):
+	sprite.global_rotation_degrees = 0
+	if abs(rotation_degrees) > 90:
+		sprite.flip_h = true
+	else:
+		sprite.flip_h = false
