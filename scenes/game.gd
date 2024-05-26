8598ddfd6b7d3b81c26239da074d823f5ab551da
vib <vibrunazo@gmail.com>
2024-05-26 11:27:24 -0300

    zoom out

--- scenes/game.gd
+++ scenes/game.gd
@@ -2,7 +2,7 @@ class_name Game
 extends Node2D
 
 @onready var artstand: ArtStand = %Artstand
-@onready var game_cam: Camera2D = $GameCam
+@onready var game_cam: GameCam = $GameCam
 
 func _ready():
 	print('game started')
@@ -13,4 +13,4 @@ func bind_signals():
 
 func _on_artstand_clicked():
 	print('art clicked')
-	game_cam.focus_on(artstand)
+	game_cam.toggle_zoom(artstand)
--- scenes/game_cam.gd
+++ scenes/game_cam.gd
@@ -6,6 +6,10 @@ var ini_zoom: Vector2
 ## default global_position to reset to on zoom out
 var ini_pos: Vector2
 
+var is_zoomed_in: bool = false
+
+signal zoom_finished
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,10 +19,14 @@ func setup_camera():
 	ini_pos = global_position
 	ini_zoom = zoom
 
+func toggle_zoom(target_node: Node2D):
+	if is_zoomed_in: zoom_out()
+	else: focus_on(target_node)
 
 var zoom_tween: Tween
 func focus_on(target_node: Node2D):
 	zoom_to(target_node.global_position, Vector2(1,1))
+	is_zoomed_in = true
 
 func zoom_to(target_pos: Vector2, target_zoom: Vector2, tween_time: float = 1.5):
 	zoom_tween = create_tween()
@@ -27,6 +35,11 @@ func zoom_to(target_pos: Vector2, target_zoom: Vector2, tween_time: float = 1.5)
 	zoom_tween.set_trans(Tween.TRANS_QUAD)
 	zoom_tween.tween_property(self, "zoom", target_zoom, tween_time)
 	zoom_tween.tween_property(self, "global_position", target_pos, tween_time)
+	zoom_tween.tween_callback(_on_zoom_finished())
+
+func _on_zoom_finished():
+	zoom_finished.emit()
 
 func zoom_out():
 	zoom_to(ini_pos, ini_zoom)
+	is_zoomed_in = false
