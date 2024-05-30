ee1edd79c95ea4ab1ea09eb1f565f2c1baa88ec0
vib <vibrunazo@gmail.com>
2024-05-30 14:40:42 -0300

    fix error on log caused by fullscreen label

--- scenes/fullscreen_label.gd
+++ scenes/fullscreen_label.gd
@@ -3,8 +3,8 @@ extends RichTextLabel
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	UI.fullscreen_toggle.connect(_on_fullscreen_toggled)
-	#_on_fullscreen_toggled(UI.is_fullscreen())
+	#UI.fullscreen_toggle.connect(_on_fullscreen_toggled)
+	_on_fullscreen_toggled()
 	get_viewport().size_changed.connect(_on_fullscreen_toggled)
 
 func _on_fullscreen_toggled():
