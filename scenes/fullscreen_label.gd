f0d38da31e4a8dad96e6738affc35ffa3ff82f56
vib <vibrunazo@gmail.com>
2024-05-30 14:46:16 -0300

    check for fullscreen toggle every tick

--- scenes/fullscreen_label.gd
+++ scenes/fullscreen_label.gd
@@ -2,10 +2,13 @@ extends RichTextLabel
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	#UI.fullscreen_toggle.connect(_on_fullscreen_toggled)
+#func _ready():
+	##UI.fullscreen_toggle.connect(_on_fullscreen_toggled)
+	#_on_fullscreen_toggled()
+	#get_viewport().size_changed.connect(_on_fullscreen_toggled)
+
+func _process(_delta):
 	_on_fullscreen_toggled()
-	get_viewport().size_changed.connect(_on_fullscreen_toggled)
 
 func _on_fullscreen_toggled():
 	visible = !UI.is_fullscreen()
