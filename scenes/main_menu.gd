95811f70949a074bebb8aed752e1dc14c85c2fe7
vib <vibrunazo@gmail.com>
2024-06-02 18:55:10 -0300

    can skip main menu anim

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -6,3 +6,8 @@ extends CanvasLayer
 func _ready():
 	#await get_tree().create_timer(1).timeout
 	anim.play("start")
+
+func _unhandled_input(event):
+	if event is InputEventMouseButton and event.is_pressed():
+		anim.play("skip")
+
