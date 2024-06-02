cf424a58fc6420c349e01647338479f2806ccfbc
vib <vibrunazo@gmail.com>
2024-06-02 15:11:01 -0300

    skip intro button

---
+++ scenes/button_skip.gd
@@ -0,0 +1,10 @@
+extends Button
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pressed.connect(_on_pressed)
+
+func _on_pressed():
+	print('skipping')
+	UI.go_to_main_menu()
