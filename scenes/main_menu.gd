e41710f196c890e68f37ba863a3628e75b63a7ec
vib <vibrunazo@gmail.com>
2024-06-02 14:48:55 -0300

    add voiceover

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -4,6 +4,5 @@ extends CanvasLayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-	await get_tree().create_timer(1).timeout
+	#await get_tree().create_timer(1).timeout
 	anim.play("start")
